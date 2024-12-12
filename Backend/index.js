9001340b1b35e3d32a53bbf4731b640db519e3e0
akshay gohil <akshaygohil.official@gmail.com>
2024-12-13 03:09:00 +0530

    Mern law system project

---
+++ Backend/index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/auth');
+const sectionRoute = require("./routes/section.route");
+const User = require('./models/user.model');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const monogoURI =  process.env.MONGO_URI
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use('/auth', authRoutes);
+app.use('/', sectionRoute);
+app.use('/', authRoutes);
+app.use('/:userId', authRoutes);
+// Use Your MongoDB Atlas OR Compass URL
+mongoose.connect(monogoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
